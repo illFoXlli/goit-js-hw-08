655a23cb27f7d9fc9f4f30ea3eb88f1334685a48
Denis <fox.ruden@gmail.com>
2022-08-10 23:16:39 +0300

    save

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,3 +2,5 @@ var Vimeo = require('vimeo').Vimeo;
 const videoPlayer = document.body.querySelector('#vimeo-player');
  videoPlayer = new Vimeo(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN);
 console.log('test');
+
+console.log("hello");
