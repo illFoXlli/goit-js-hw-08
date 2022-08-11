c303b379d68142c66acc98c1aa75d264fbd46dea
Denis <fox.ruden@gmail.com>
2022-08-12 01:57:42 +0300

    sleepless night

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,56 +1,56 @@
 
 
 import Player from '@vimeo/player';
-// import {add, throttle} from 'lodash';
-import  'lodash.throttle';
-// var _ = require('lodash');
+import throttle from 'lodash.throttle';
+
 
 const videoIframe = document.querySelector('#vimeo-player');
 const player = new Player(videoIframe);
 const SAVE_CURRENT_TIME_VIDEO = "videoplayer-current-time";
 
-player.on('timeupdate', function (even) {
-  console.log(even);
-localStorage.setItem(SAVE_CURRENT_TIME_VIDEO, even.seconds);
-});
+player.on(
+  'timeupdate',
+  throttle(even => {
+    console.log(even);
+    localStorage.setItem(SAVE_CURRENT_TIME_VIDEO, even.seconds);
+  }, 3000)
+);
 
 
 player
   .setCurrentTime(localStorage.getItem(SAVE_CURRENT_TIME_VIDEO))
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        console.log("Видео еще не воспроизводилось!");
+        break;
+
+      default:
+        // some other error occurred
+        console.log("какаята ошибка неизвестная");
+        break;
+    }
+  }
+  );;
+
+
+// const result = add(2, 3);
+// console.log(result);
+//   // .then(function (seconds) {
+  //   // seconds = the actual time that the player seeked to
+  // })
   // .catch(function (error) {
   //   switch (error.name) {
   //     case 'RangeError':
   //       // the time was less than 0 or greater than the video’s duration
-  //       console.log("Видео еще не воспроизводилось!");
   //       break;
 
   //     default:
   //       // some other error occurred
-  //       console.log("какаята ошибка неизвестная");
   //       break;
   //   }
-  // }
-  // );;
-
-
-const result = add(2, 3);
-console.log(result);
-
-
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
-
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
+  // });
 
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector("[name='email']"),
+  message: document.querySelector("[name='message']"),
+};
+
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
+let obj = {};
+let obj2 = {};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(event) {
+  obj = {
+    email: refs.inputEmail.value,
+    message: refs.message.value,
+  };
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(obj));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    obj = obj2;
+  if (refs.inputEmail.value.trim() !== '' && refs.message.value.trim() !== '') {
+      console.log(obj);
+      
+    localStorage.removeItem(FEEDBACK_FORM_STATE);
+    event.target.reset();
+  }
+}
+
+function reset() {
+  if (JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE))) {
+    obj2 = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE));
+
+    refs.inputEmail.value = obj2.email;
+    refs.message.value = obj2.message;
+  }
+}
+
+reset();
