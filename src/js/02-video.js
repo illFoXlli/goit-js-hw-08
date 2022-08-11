e4b13a4d50de5deb0c69d6a2f310888c3fccae99
Denis <fox.ruden@gmail.com>
2022-08-11 19:41:36 +0300

    bad

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,56 @@
-var Vimeo = require('vimeo').Vimeo;
-const videoPlayer = document.body.querySelector('#vimeo-player');
- videoPlayer = new Vimeo(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN);
-console.log('test');
 
-console.log("hello");
+
+import Player from '@vimeo/player';
+// import {add, throttle} from 'lodash';
+import  'lodash.throttle';
+// var _ = require('lodash');
+
+const videoIframe = document.querySelector('#vimeo-player');
+const player = new Player(videoIframe);
+const SAVE_CURRENT_TIME_VIDEO = "videoplayer-current-time";
+
+player.on('timeupdate', function (even) {
+  console.log(even);
+localStorage.setItem(SAVE_CURRENT_TIME_VIDEO, even.seconds);
+});
+
+
+player
+  .setCurrentTime(localStorage.getItem(SAVE_CURRENT_TIME_VIDEO))
+  // .catch(function (error) {
+  //   switch (error.name) {
+  //     case 'RangeError':
+  //       // the time was less than 0 or greater than the video’s duration
+  //       console.log("Видео еще не воспроизводилось!");
+  //       break;
+
+  //     default:
+  //       // some other error occurred
+  //       console.log("какаята ошибка неизвестная");
+  //       break;
+  //   }
+  // }
+  // );;
+
+
+const result = add(2, 3);
+console.log(result);
+
+
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
+
+
+
